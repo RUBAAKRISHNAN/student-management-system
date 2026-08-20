ec45132541d92b25d38d83aef47d2d76aba52b9a
RUBAAKRISHNAN K <gtkrish1906@gamil.com>
2026-08-20 22:12:13 +0530

    cors changed

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,41 @@
-const express=require('express');
-const cors=require("cors");
-const mongoose=require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
+
 const studentRoutes = require("./routes/studentRoutes");
+const authRouter = require("./routes/authRoutes");
 
- const authRouter=require('./routes/authRoutes');
+const app = express();
 
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://student-management-system-liart-zeta.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-const app=express();
-app.use(cors());
 app.use(express.json());
+
 app.use("/api/students", studentRoutes);
+app.use("/api/auth", authRouter);
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
- app.use("/api/auth",authRouter)
+app.get("/", (req, res) => {
+  res.send("Hello World");
+  console.log("Hello World");
+});
 
-
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-      console.log("MongoDB connected");
-}).catch((err)=>{
-      console.log(err);
-})
-
- app.get('/',(req,res)=>{
-    res.send("Hello World");
-    console.log("Hello World");
- })
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
